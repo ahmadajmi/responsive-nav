8204f9e7266936dd3d14aaf1be8ad4594d6e7f57
Ahmad Ajmi <ahmadajmi@outlook.com>
2013-12-19 00:03:38 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+
+	<!-- Meta Tags -->
+	<meta charset="utf-8" />
+
+	<title>Responsive Menu</title>
+
+	<meta name="description" content="A Simple Responsive Menu" /> 
+
+	<!-- Mobile Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Style -->
+	<link rel="stylesheet" href="_include/css/style.css" />
+
+	<!-- Modernizr -->
+	<script src="_include/js/vendor/custom.modernizr.js"></script>
+
+</head>
+
+<body id="home">
+
+	<!-- Start Header Section -->
+	<section id="header-section" class="header-section stick">
+		<div class="row">
+
+			<a href="#" class="nav-toggle">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</a>
+
+			<div class="logo left">
+				<a href="index.php" title="Responsive Menu" class="transition">AJMI</a>
+			</div>
+
+			<nav class="right">
+				<ul class="nav main-nav">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Services</a></li>
+					<li><a href="#">Work</a></li>
+					<li><a href="#">Pricing</a></li>
+					<li><a href="#">Team</a></li>
+					<li><a href="#">Clients</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</nav>
+
+		</div>
+	</section>
+	<!-- End Header Section -->
+
+	<!-- JavaScript -->
+
+	<!-- jQuery -->
+	<script src="_include/js/vendor/jquery.js"></script>
+
+	<!-- Main -->
+	<script src="_include/js/main.js"></script>
+
+	<!-- End JavaScript -->
+	
+</body>
+</html>
