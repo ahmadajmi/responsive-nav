62a6626374e5b05f34943e03b3af1d34d022f37e
Ahmad Ajmi <ahmadajmi@outlook.com>
2013-12-20 18:16:59 +0200

    Removed unused scripts

--- _include/js/main.js
+++ _include/js/main.js
@@ -1,18 +1,9 @@
 jQuery(function ($) {
 
-    /* =============    Header          ============= */
-
     // Toggle navigation
     $('.nav-toggle').click(function (e) {
         e.preventDefault();
         $('.main-nav').toggleClass('active-nav');
-    });
-    
-    // Toggle .active class, so we can add a white background instead of transparent when scroll
-    $(document).scroll(function () {
-        var $header_section = $('#header-section');
-        ($(window).scrollTop() > 100 ? $header_section.addClass('active') : $header_section.removeClass('active'));
-
     });
 
 });
